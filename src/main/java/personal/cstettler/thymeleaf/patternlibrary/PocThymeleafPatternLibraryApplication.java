cddec14245b0a41ffab6ff1dc3863a97eacdedfe
cstettler <christian.stettler@innoq.com>
2022-11-01 11:45:37 +0100

    scaffold new maven project

---
+++ src/main/java/personal/cstettler/thymeleaf/patternlibrary/PocThymeleafPatternLibraryApplication.java
@@ -0,0 +1,13 @@
+package personal.cstettler.thymeleaf.patternlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocThymeleafPatternLibraryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocThymeleafPatternLibraryApplication.class, args);
+  }
+
+}
