8644aae7819995fabdd5969200ffd019243e379d
cstettler <christian.stettler@innoq.com>
2022-11-11 09:30:04 +0100

    add toggling chevron icon to collapsible

--- poc-thymeleaf-pattern-library-components/src/main/resources/templates/components/collapsible/collapsible.js
+++ poc-thymeleaf-pattern-library-components/src/main/resources/templates/components/collapsible/collapsible.js
@@ -1,24 +1,42 @@
 export class Collapsible extends HTMLElement {
   connectedCallback() {
     const initiallyOpen = this.getAttribute('open') !== null;
-    this.titleElement = this.contents.item(1);
-    this.contentElement = this.contents.item(2);
 
     this.titleClickHandler = () => {
       this.contentElement.hidden = !this.contentElement.hidden;
+      this.updateArrow();
     };
 
     this.titleElement.addEventListener('click', this.titleClickHandler)
 
     this.contentElement.hidden = !initiallyOpen;
+    this.updateArrow();
   }
 
   disconnectedCallback() {
     this.titleElement.removeEventListener('click', this.titleClickHandler)
   }
 
-  get contents() {
-    return this.querySelectorAll('*');
+  updateArrow() {
+    if (this.contentElement.hidden) {
+      this.arrowElement.classList.remove('pl-collapsible-arrow-expanded');
+      this.arrowElement.classList.add('pl-collapsible-arrow-collapsed');
+    } else {
+      this.arrowElement.classList.remove('pl-collapsible-arrow-collapsed');
+      this.arrowElement.classList.add('pl-collapsible-arrow-expanded');
+    }
+  }
+
+  get titleElement() {
+    return this.querySelector(".pl-collapsible-title");
+  }
+
+  get arrowElement() {
+    return this.querySelector(".pl-collapsible-arrow");
+  }
+
+  get contentElement() {
+    return this.querySelector(".pl-collapsible-content");
   }
 }
 
