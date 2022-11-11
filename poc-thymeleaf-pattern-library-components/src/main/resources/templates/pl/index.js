485cd78a0243e0304cf75cb17035ae31389e1ebe
cstettler <christian.stettler@innoq.com>
2022-11-11 16:22:22 +0100

    provide convenience exports for registering globals and stimulus controllers

--- poc-thymeleaf-pattern-library-components/src/main/resources/templates/pl/index.js
+++ poc-thymeleaf-pattern-library-components/src/main/resources/templates/pl/index.js
@@ -1,8 +1,18 @@
+import { Application as StimulusApplication } from "stimulus";
 import { AlertController } from './alert/alert.js'
 import { greet } from './button/button.js'
 import './collapsible/collapsible.js'
 
 export {
-  AlertController,
-  greet
+  StimulusApplication,
+  registerGlobals,
+  registerStimulusControllers,
+}
+
+function registerGlobals(container) {
+  container.greet = greet;
+}
+
+function registerStimulusControllers(stimulusApplication) {
+  stimulusApplication.register('alert', AlertController)
 }
