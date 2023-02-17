aa9a3efa74b5a5de8a7e1137069d0e916b8bbeee
cstettler <christian.stettler@innoq.com>
2023-02-17 17:58:26 +0100

    add minimal setup for playwright-based integration tests

--- poc-thymeleaf-pattern-library-application/src/test/java/personal/cstettler/thymeleaf/patternlibrary/PocThymeleafPatternLibraryApplicationTests.java
+++ poc-thymeleaf-pattern-library-application/src/test/java/personal/cstettler/thymeleaf/patternlibrary/PocThymeleafPatternLibraryApplicationTests.java
@@ -1,13 +1,54 @@
 package personal.cstettler.thymeleaf.patternlibrary;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestPropertySource(properties = {
+	"pattern-library.components-resource-path=classpath:/test"
+})
 class PocThymeleafPatternLibraryApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private int localServerPort;
+
+	private Playwright playwright;
+	private Browser browser;
+	private BrowserContext context;
+	private Page page;
+
+	@BeforeEach
+	void setupPlaywright() {
+		playwright = Playwright.create();
+		browser = playwright.chromium().launch();
+		context = browser.newContext(new Browser.NewContextOptions().setBaseURL("http://localhost:" + localServerPort));
+		page = context.newPage();
 	}
 
+	@AfterEach
+	void tearDownPlaywright() {
+		page.close();
+		context.close();
+		browser.close();
+		playwright.close();
+	}
+
+	@Test
+	void applicationStartsSuccessfully() {
+		page.navigate("/");
+		String headerTitle = page.querySelector("header").textContent().trim();
+
+		assertEquals("Thymeleaf Component Library (PoC)", headerTitle);
+	}
 }
